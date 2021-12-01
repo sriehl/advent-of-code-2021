8289a544d31a54d8716a42ae976362dcff85e278
Steven Riehl <sr@stevenriehl.com>
2021-12-01 15:50:10 +0000

    day 1 part 1 done

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,8 +1,35 @@
 defmodule AdventOfCode.Day01 do
   @moduledoc false
-  def part1(_args) do
+
+  defp check_for_increase(readings, count \\ 0)
+  defp check_for_increase([], count), do: count
+  defp check_for_increase([_current | rest], count) when rest == [], do: count
+
+  defp check_for_increase([current | rest], count) do
+    if current < List.first(rest) do
+      check_for_increase(rest, count + 1)
+    else
+      check_for_increase(rest, count)
+    end
   end
 
-  def part2(_args) do
+  @spec part1(String.t()) :: integer()
+  def part1(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> check_for_increase()
+  end
+
+  @spec part2(String.t()) :: integer()
+  def part2(input) do
+    depths =
+      input
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(&String.to_integer/1)
+
+    0
   end
 end
